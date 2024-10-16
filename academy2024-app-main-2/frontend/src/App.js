c76398ba9ebced5cdb50e1031b1afb6706647720
Yusuf Hamza Kıran <143087222+yusufhkran@users.noreply.github.com>
2024-10-16 15:55:46 +0300

    Update App.js

--- academy2024-app-main-2/frontend/src/App.js
+++ academy2024-app-main-2/frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const fetchGreeting = async () => {
     try {
-      const response = await axios.get(`http://internal-k8s-kubesyst-sprintin-c5c14cefce-1796415880.us-east-1.elb.amazonaws.com/:80/${name}`);
+      const response = await axios.get(`http://172.20.58.68:80/${name}`);
       setGreeting(response.data.message);
     } catch (error) {
       console.error('Mesaj alınırken hata oluştu:', error);
